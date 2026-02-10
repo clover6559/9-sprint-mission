e3299dc09ed884f041fc07ad894e4ac3be836e6d
박지은 <clover6559@gmail.com>
2026-02-10 18:43:13 +0900

    feat(binaryContent): 바이너리 파일 다운로드 구현     하나 또는 여러개 조회 기능 포함

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,20 +1,43 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/binarycontent")
+@RequiredArgsConstructor
 public class BinaryContentController {
-//    private Optional<BinaryContentCreateRequest> resolveProfileRequest(MultipartFile profileFile) {
-//        if (profileFile.isEmpty()) {
-//            return Optional.empty();
-//        } else {
-//            try {
-//                BinaryContentCreateRequest binaryContentCreateRequest = new BinaryContentCreateRequest(
-//                        profileFile.getOriginalFilename(),
-//                        profileFile.getContentType(),
-//                        profileFile.getBytes()
-//                );
-//                return Optional.of(binaryContentCreateRequest);
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//    }
+    private final BinaryContentService binaryContentService;
+
+    @RequestMapping(
+            path = "/find/{uuidList}",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<List<BinaryContent>> findAll(
+            @RequestParam ("ids") List<UUID> uuidList
+            ){
+                if (uuidList == null || uuidList.isEmpty()) {
+                    return ResponseEntity.badRequest().build();
+                }
+        List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(uuidList);
+        return ResponseEntity.ok(binaryContents);
+    }
+
+    @RequestMapping(
+            path = "/{id}",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<BinaryContent> find(
+            @PathVariable UUID id
+    ){
+        BinaryContent binaryContent = binaryContentService.find(id);
+        return ResponseEntity.ok(binaryContent);
+    }
+
 }
