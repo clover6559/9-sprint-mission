b8435b7a1d5d48bfc79e57d185d38e60db4d0540
박지은 <clover6559@gmail.com>
2026-02-20 18:33:48 +0900

    1

--- discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,31 +1,15 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.dto.channel.*;
-import com.sprint.mission.discodeit.dto.message.MessageCreate;
-import com.sprint.mission.discodeit.dto.message.MessageUpdate;
-import com.sprint.mission.discodeit.dto.user.UserCreate;
-import com.sprint.mission.discodeit.dto.user.UserFind;
-import com.sprint.mission.discodeit.dto.user.UserUpdate;
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.search.ChannelSearch;
-import com.sprint.mission.discodeit.service.search.MessageSearch;
-import com.sprint.mission.discodeit.service.search.UserSearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.List;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-	public static void main(String[] args) {
-				SpringApplication.run(DiscodeitApplication.class, args);
 
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DiscodeitApplication.class, args);
+
+  }
 
 }
