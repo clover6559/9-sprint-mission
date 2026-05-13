93e5d60fa1c90aa6c4d844341f96277d4146c710
jieun <clover6559@gmail.com>
2026-05-13 17:00:11 +0900

    feat: 사용자 권한(Role) 엔티티 추가 및 DB 스키마 반영

--- src/main/java/com/sprint/mission/discodeit/dto/request/UserRoleUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserRoleUpdateRequest.java
@@ -1,4 +1,8 @@
 package com.sprint.mission.discodeit.dto.request;
 
-public record UserRoleUpdateRequest() {
+import com.sprint.mission.discodeit.entity.Role;
+import java.util.UUID;
+
+public record UserRoleUpdateRequest(UUID userId, Role newRole) {
+
 }
