643cadc88f65451bc361419331349bca03062d9b
jieun <clover6559@gmail.com>
2026-06-15 17:19:27 +0900

    feat: 메시지 생성 이벤트 발생 시 웹소켓으로 메시지를 전송하는 리스너 구현

---
+++ src/main/java/com/sprint/mission/discodeit/event/WebSocketRequiredEventListener.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.dto.data.MessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketRequiredEventListener {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleMessage(MessageCreatedEvent event) {
+        UUID channelId = event.channelId();
+        String messageDto = event.content();
+        String destination = "/sub/channels." + channelId + ".messages";
+
+        messagingTemplate.convertAndSend(destination, messageDto);
+    }
+}
