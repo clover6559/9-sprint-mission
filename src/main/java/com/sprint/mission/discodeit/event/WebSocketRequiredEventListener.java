6f7f658ea5597544f02678869d5941475129ea8a
jieun <clover6559@gmail.com>
2026-06-16 10:49:21 +0900

    feat: 파일 업로드 상태 변경 시 binaryContents.updated SSE 이벤트 전송

--- src/main/java/com/sprint/mission/discodeit/event/WebSocketRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/WebSocketRequiredEventListener.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.event;
 
-import com.sprint.mission.discodeit.dto.data.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
