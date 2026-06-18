9dcabebacd5000afa87dc01d1602a06b30672fd0
jieun <clover6559@gmail.com>
2026-06-18 18:04:07 +0900

    feat: WebSocket 메시지 전송을 Kafka Producer 방식으로 리팩토링

--- src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
@@ -2,11 +2,11 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.data.MessageDto;
 import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
+import com.sprint.mission.discodeit.event.SseEventProducer;
 import com.sprint.mission.discodeit.service.basic.BasicMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 
@@ -14,9 +14,12 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class MessageWebSocketController {
     private final BasicMessageService messageService;
+    private final SseEventProducer sseEventProducer;
+
+
     @MessageMapping("/messages")
-    @SendTo("/sub/messages")
-    public MessageDto sendMessage(@Payload MessageCreateRequest request) {
-        return messageService.create(request, null);
+    public void sendMessage(@Payload MessageCreateRequest request) {
+        MessageDto result = messageService.create(request, null);
+        sseEventProducer.broadcast("messages.created", result);
     }
 }
